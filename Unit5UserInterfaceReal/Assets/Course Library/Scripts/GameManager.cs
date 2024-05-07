5cbf508bb0e8bab573480227bda281daed43569c
Xavier Real <720838@student.dusd.net>
2024-05-07 15:24:24 -0700

    Almost done
    :( almost done with rip off of fruit ninja

--- Unit5UserInterfaceReal/Assets/Course Library/Scripts/GameManager.cs
+++ Unit5UserInterfaceReal/Assets/Course Library/Scripts/GameManager.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System.Runtime.CompilerServices;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     private float spawnRate = 1.0f;
+    private int score;
+    public TextMeshProUGUI scoreText;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        UpdateScore(0);
     }
 
     // Update is called once per frame
@@ -20,11 +25,17 @@ public class GameManager : MonoBehaviour
     }
     IEnumerator SpawnTarget ()
     {
-        while (true)
+        while (true) 
         {
+            UpdateScore(5);
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
         }
+    }
+    private void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 }
