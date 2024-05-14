5028f4b8bea28c129756222e5dbcaf1dbe877a72
Xavier Real <720838@student.dusd.net>
2024-05-13 22:42:11 -0700

    I AM DONE
    I AM DONE :DDD

--- Unit5UserInterfaceReal/Assets/Course Library/Scripts/GameManager.cs
+++ Unit5UserInterfaceReal/Assets/Course Library/Scripts/GameManager.cs
@@ -8,6 +8,10 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject pauseScreen;
+    private bool paused;
+    public TextMeshProUGUI livesText;
+    private int lives;
     public Button restartButton;
     public List<GameObject> targets;
     private float spawnRate = 1.0f;
@@ -28,12 +32,16 @@ public class GameManager : MonoBehaviour
         score = 0;
         UpdateScore(0);
         titleScreen.gameObject.SetActive(false);
+        UpdateLives(3);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ChangePaused();
+        }
     }
     IEnumerator SpawnTarget ()
     {
@@ -58,5 +66,29 @@ public class GameManager : MonoBehaviour
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void UpdateLives(int livesToChange)
+    {
+        lives += livesToChange;
+        livesText.text = "Lives: " + lives;
+        if (lives <=0)
+        {
+            GameOver();
+        }
+    }
+    void ChangePaused()
+    {
+        if(!paused)
+        {
+            paused = true;
+            pauseScreen.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            paused = false;
+            pauseScreen.SetActive(false);
+            Time.timeScale = 1;
+        }
     }
 }
--- Unit5UserInterfaceReal/Assets/Course Library/Scripts/Target.cs
+++ Unit5UserInterfaceReal/Assets/Course Library/Scripts/Target.cs
@@ -58,7 +58,7 @@ public class Target : MonoBehaviour
         Destroy(gameObject);
         if (!gameObject.CompareTag("Bad"))
         {
-            gameManager.GameOver();
+            gameManager.UpdateLives(-1);
         }
     }
 }
